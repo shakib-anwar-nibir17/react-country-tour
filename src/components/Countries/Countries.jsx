7c7a2bfaa5f219e64573003b458ec8167428dc5e
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-09-14 01:42:05 +0600

    Updating state in arrays and pass parameter

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -10,12 +10,30 @@ const Countries = () => {
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
+
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const handleVisitedCountries = (country) => {
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
   return (
     <div>
       <h3>Countries: {countries.length} </h3>
+      <div>
+        <h3>Visited Countries:{visitedCountries.length} </h3>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
       <div className="country-container">
         {countries.map((country) => (
-          <Country key={country.cca3} country={country}></Country>
+          <Country
+            key={country.cca3}
+            handleVisitedCountries={handleVisitedCountries}
+            country={country}
+          ></Country>
         ))}
       </div>
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
   const { name, flags, population, area, cca3 } = country;
 
   const [visited, setVisited] = useState(false);
@@ -8,14 +8,19 @@ const Country = ({ country }) => {
     setVisited(!visited);
   };
   return (
-    <div className="country">
-      <h3>Name: {name.common}</h3>
+    <div className={`country ${visited && "visited"}`}>
+      <h3 style={{ color: visited ? "red" : "black" }}>{name.common}</h3>
       <img src={flags.png} alt="" />
       <p>Population: {population}</p>
       <p>Area: {area}</p>
       <p>
         <small>Code:{cca3}</small>
       </p>
+      <button className="btn" onClick={() => handleVisitedCountries(country)}>
+        Mark Visited
+      </button>
+      <br />
+      <br />
       <button className="btn" onClick={handleVisited}>
         {visited ? "Visited" : "Going"}
       </button>
